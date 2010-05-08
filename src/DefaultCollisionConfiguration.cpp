bd67d1b59fa0207e16528dcdd316a901db6d0b11
andres.traks <andres.traks@030a5c38-1ffe-11df-902a-156ec2e33ad3>
2010-05-08 23:31:30 +0000

    - Complete DefaultCollisionConstructionInfo.

--- src/DefaultCollisionConfiguration.cpp
+++ src/DefaultCollisionConfiguration.cpp
@@ -2,9 +2,103 @@
 
 #include "CollisionConfiguration.h"
 #include "DefaultCollisionConfiguration.h"
+#include "PoolAllocator.h"
+#include "StackAlloc.h"
+
+DefaultCollisionConstructionInfo::DefaultCollisionConstructionInfo()
+{
+	_constructionInfo = new btDefaultCollisionConstructionInfo();
+}
+
+PoolAllocator^ DefaultCollisionConstructionInfo::CollisionAlgorithmPool::get()
+{
+	return gcnew PoolAllocator(_constructionInfo->m_collisionAlgorithmPool);
+}
+void DefaultCollisionConstructionInfo::CollisionAlgorithmPool::set(PoolAllocator^ value)
+{
+	_constructionInfo->m_collisionAlgorithmPool = value->UnmanagedPointer;
+}
+
+int DefaultCollisionConstructionInfo::CustomCollisionAlgorithmMaxElementSize::get()
+{
+	return _constructionInfo->m_customCollisionAlgorithmMaxElementSize;
+}
+void DefaultCollisionConstructionInfo::CustomCollisionAlgorithmMaxElementSize::set(int value)
+{
+	_constructionInfo->m_customCollisionAlgorithmMaxElementSize = value;
+}
+
+int DefaultCollisionConstructionInfo::DefaultMaxCollisionAlgorithmPoolSize::get()
+{
+	return _constructionInfo->m_defaultMaxCollisionAlgorithmPoolSize;
+}
+void DefaultCollisionConstructionInfo::DefaultMaxCollisionAlgorithmPoolSize::set(int value)
+{
+	_constructionInfo->m_defaultMaxCollisionAlgorithmPoolSize = value;
+}
+
+int DefaultCollisionConstructionInfo::DefaultMaxPersistentManifoldPoolSize::get()
+{
+	return _constructionInfo->m_defaultMaxPersistentManifoldPoolSize;
+}
+void DefaultCollisionConstructionInfo::DefaultMaxPersistentManifoldPoolSize::set(int value)
+{
+	_constructionInfo->m_defaultMaxPersistentManifoldPoolSize = value;
+}
+
+int DefaultCollisionConstructionInfo::DefaultStackAllocatorSize::get()
+{
+	return _constructionInfo->m_defaultStackAllocatorSize;
+}
+void DefaultCollisionConstructionInfo::DefaultStackAllocatorSize::set(int value)
+{
+	_constructionInfo->m_defaultStackAllocatorSize = value;
+}
+
+PoolAllocator^ DefaultCollisionConstructionInfo::PersistentManifoldPool::get()
+{
+	return gcnew PoolAllocator(_constructionInfo->m_persistentManifoldPool);
+}
+void DefaultCollisionConstructionInfo::PersistentManifoldPool::set(PoolAllocator^ value)
+{
+	_constructionInfo->m_persistentManifoldPool = value->UnmanagedPointer;
+}
+
+StackAlloc^ DefaultCollisionConstructionInfo::StackAllocator::get()
+{
+	return gcnew StackAlloc(_constructionInfo->m_stackAlloc);
+}
+void DefaultCollisionConstructionInfo::StackAllocator::set(StackAlloc^ value)
+{
+	_constructionInfo->m_stackAlloc = value->UnmanagedPointer;
+}
+
+int DefaultCollisionConstructionInfo::UseEpaPenetrationAlgorithm::get()
+{
+	return _constructionInfo->m_useEpaPenetrationAlgorithm;
+}
+void DefaultCollisionConstructionInfo::UseEpaPenetrationAlgorithm::set(int value)
+{
+	_constructionInfo->m_useEpaPenetrationAlgorithm = value;
+}
+
+btDefaultCollisionConstructionInfo* DefaultCollisionConstructionInfo::UnmanagedPointer::get()
+{
+	return _constructionInfo;
+}
+void DefaultCollisionConstructionInfo::UnmanagedPointer::set( btDefaultCollisionConstructionInfo* value )
+{
+	_constructionInfo = value;
+}
+
 
 DefaultCollisionConfiguration::DefaultCollisionConfiguration(btDefaultCollisionConfiguration* conf)
 : CollisionConfiguration(conf)
+{
+}
+
+DefaultCollisionConfiguration::DefaultCollisionConfiguration(DefaultCollisionConstructionInfo^ constructionInfo)
+: CollisionConfiguration(new btDefaultCollisionConfiguration(*constructionInfo->UnmanagedPointer))
 {
 }
 
--- src/DefaultCollisionConfiguration.h
+++ src/DefaultCollisionConfiguration.h
@@ -1,16 +1,84 @@
 #pragma once
 
+// Fully implemented as of 09 May 2010
+
 #include "CollisionConfiguration.h"
 
 namespace BulletSharp
 {
+	public ref class DefaultCollisionConstructionInfo
+	{
+	private:
+		btDefaultCollisionConstructionInfo* _constructionInfo;
+
+	public:
+		DefaultCollisionConstructionInfo();
+
+		property PoolAllocator^ CollisionAlgorithmPool
+		{
+			PoolAllocator^ get();
+			void set(PoolAllocator^ value);
+		}
+
+		property int CustomCollisionAlgorithmMaxElementSize
+		{
+			int get();
+			void set(int value);
+		}
+
+		property int DefaultMaxCollisionAlgorithmPoolSize
+		{
+			int get();
+			void set(int value);
+		}
+
+		property int DefaultMaxPersistentManifoldPoolSize
+		{
+			int get();
+			void set(int value);
+		}
+
+		property int DefaultStackAllocatorSize
+		{
+			int get();
+			void set(int value);
+		}
+
+		property PoolAllocator^ PersistentManifoldPool
+		{
+			PoolAllocator^ get();
+			void set(PoolAllocator^ value);
+		}
+
+		property StackAlloc^ StackAllocator
+		{
+			StackAlloc^ get();
+			void set(StackAlloc^ value);
+		}
+
+		property int UseEpaPenetrationAlgorithm
+		{
+			int get();
+			void set(int value);
+		}
+
+	internal:
+		property btDefaultCollisionConstructionInfo* UnmanagedPointer
+		{
+			virtual btDefaultCollisionConstructionInfo* get();
+			void set(btDefaultCollisionConstructionInfo* value);
+		}
+	};
+
 	public ref class DefaultCollisionConfiguration : CollisionConfiguration
 	{
 	internal:
 		DefaultCollisionConfiguration(btDefaultCollisionConfiguration* conf);
 	
 	public:
+		DefaultCollisionConfiguration(DefaultCollisionConstructionInfo^ constructionInfo);
 		DefaultCollisionConfiguration();
+
 		void SetConvexConvexMultipointIterations(int numPerturbationIterations);
 		void SetConvexConvexMultipointIterations(int numPerturbationIterations, int minimumPointsPerturbationThreshold);
 
