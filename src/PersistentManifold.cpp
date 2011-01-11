ee45a1934a5367d4e4fe03c19d4899f9e331c68f
andres.traks@gmail.com <andres.traks@gmail.com@030a5c38-1ffe-11df-902a-156ec2e33ad3>
2011-01-11 18:02:22 +0000

    - Work on PersistentManifold.

--- src/PersistentManifold.cpp
+++ src/PersistentManifold.cpp
@@ -1,5 +1,6 @@
 #include "StdAfx.h"
 
+#include "ManifoldPoint.h"
 #include "PersistentManifold.h"
 
 PersistentManifold::PersistentManifold(btPersistentManifold* manifold)
@@ -10,6 +11,104 @@ PersistentManifold::PersistentManifold(btPersistentManifold* manifold)
 PersistentManifold::PersistentManifold()
 : TypedObject(new btPersistentManifold())
 {
+}
+
+int PersistentManifold::AddManifoldPoint(ManifoldPoint^ newPoint)
+{
+	return UnmanagedPointer->addManifoldPoint(*newPoint->UnmanagedPointer);
+}
+
+void PersistentManifold::ClearManifold()
+{
+	return UnmanagedPointer->clearManifold();
+}
+
+void PersistentManifold::ClearUserCache(ManifoldPoint^ pt)
+{
+	UnmanagedPointer->clearUserCache(*pt->UnmanagedPointer);
+}
+
+#ifdef DEBUG_PERSISTENCY
+void PersistentManifold::DebugPersistency()
+{
+	UnmanagedPointer->DebugPersistency();
+}
+#endif
+
+int PersistentManifold::GetCacheEntry(ManifoldPoint^ newPoint)
+{
+	return UnmanagedPointer->getCacheEntry(*newPoint->UnmanagedPointer);
+}
+
+ManifoldPoint^ PersistentManifold::GetContactPoint(int index)
+{
+	return gcnew ManifoldPoint(&UnmanagedPointer->getContactPoint(index));
+}
+
+void PersistentManifold::RefreshContactPoints(Matrix trA, Matrix trB)
+{
+	btTransform* trATemp = Math::MatrixToBtTransform(trA);
+	btTransform* trBTemp = Math::MatrixToBtTransform(trB);
+	UnmanagedPointer->refreshContactPoints(*trATemp, *trBTemp);
+	delete trATemp;
+	delete trBTemp;
+}
+
+void PersistentManifold::RemoveContactPoint(int index)
+{
+	UnmanagedPointer->removeContactPoint(index);
+}
+
+void PersistentManifold::ReplaceContactPoint(ManifoldPoint^ newPoint, int insertIndex)
+{
+	UnmanagedPointer->replaceContactPoint(*newPoint->UnmanagedPointer, insertIndex);
+}
+
+bool PersistentManifold::ValidContactDistance(ManifoldPoint^ pt)
+{
+	return UnmanagedPointer->validContactDistance(*pt->UnmanagedPointer);
+}
+
+int PersistentManifold::CompanionIDA::get()
+{
+	return UnmanagedPointer->m_companionIdA;
+}
+void PersistentManifold::CompanionIDA::set(int value)
+{
+	UnmanagedPointer->m_companionIdA = value;
+}
+
+int PersistentManifold::CompanionIDB::get()
+{
+	return UnmanagedPointer->m_companionIdB;
+}
+void PersistentManifold::CompanionIDB::set(int value)
+{
+	UnmanagedPointer->m_companionIdB = value;
+}
+
+float PersistentManifold::ContactBreakingThreshold::get()
+{
+	return UnmanagedPointer->getContactBreakingThreshold();
+}
+
+float PersistentManifold::ContactProcessingThreshold::get()
+{
+	return UnmanagedPointer->getContactProcessingThreshold();
+}
+
+int PersistentManifold::Index1A::get()
+{
+	return UnmanagedPointer->m_index1a;
+}
+void PersistentManifold::Index1A::set(int value)
+{
+	UnmanagedPointer->m_index1a = value;
+}
+
+int PersistentManifold::NumContacts::get()
+{
+	return UnmanagedPointer->getNumContacts();
 }
 
 btPersistentManifold* PersistentManifold::UnmanagedPointer::get()
--- src/PersistentManifold.h
+++ src/PersistentManifold.h
@@ -4,6 +4,8 @@
 
 namespace BulletSharp
 {
+	ref class ManifoldPoint;
+
 	public ref class PersistentManifold : TypedObject
 	{
 	internal:
@@ -11,6 +13,64 @@ namespace BulletSharp
 
 	public:
 		PersistentManifold();
+		//PersistentManifold(Object^ body0, Object^ body1, int , btScalar contactBreakingThreshold, btScalar contactProcessingThreshold);
+
+		int AddManifoldPoint(ManifoldPoint^ newPoint);
+		void ClearManifold();
+		void ClearUserCache(ManifoldPoint^ pt);
+#ifdef DEBUG_PERSISTENCY
+		void DebugPersistency();
+#endif
+		int GetCacheEntry(ManifoldPoint^ newPoint);
+		ManifoldPoint^ GetContactPoint(int index);
+		void RefreshContactPoints(Matrix trA, Matrix trB);
+		void RemoveContactPoint(int index);
+		void ReplaceContactPoint(ManifoldPoint^ newPoint, int insertIndex);
+		//void SetBodies(Object^ body0, Object^ body1);
+		bool ValidContactDistance(ManifoldPoint^ pt);
+
+		//property Object^ Body0
+		//{
+		//	Object^ get();
+		//}
+
+		//property Object^ Body1
+		//{
+		//	Object^ get();
+		//}
+
+		property int CompanionIDA
+		{
+			int get();
+			void set(int value);
+		}
+
+		property int CompanionIDB
+		{
+			int get();
+			void set(int value);
+		}
+
+		property float ContactBreakingThreshold
+		{
+			btScalar get();
+		}
+
+		property float ContactProcessingThreshold
+		{
+			btScalar get();
+		}
+
+		property int Index1A
+		{
+			int get();
+			void set(int value);
+		}
+
+		property int NumContacts
+		{
+			int get();
+		}
 
 	internal:
 		property btPersistentManifold* UnmanagedPointer
