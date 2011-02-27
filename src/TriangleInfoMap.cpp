d6f37f9c57d6a3e37b93e1f458e83876fed47d2a
andres.traks@gmail.com <andres.traks@gmail.com@030a5c38-1ffe-11df-902a-156ec2e33ad3>
2011-02-27 07:46:01 +0000

    - Implement TriangleInfoMap thresholds.

--- src/TriangleInfoMap.cpp
+++ src/TriangleInfoMap.cpp
@@ -1,10 +1,81 @@
 #include "StdAfx.h"
 
 #include "TriangleInfoMap.h"
+#ifndef DISABLE_SERIALIZE
+#include "Serializer.h"
+#endif
 
 TriangleInfoMap::TriangleInfoMap()
 {
 	_triangleInfoMap = new btTriangleInfoMap();
+}
+
+void TriangleInfoMap::Clear()
+{
+	_triangleInfoMap->clear();
+}
+
+#ifndef DISABLE_SERIALIZE
+int	TriangleInfoMap::CalculateSerializeBufferSize()
+{
+	return _triangleInfoMap->calculateSerializeBufferSize();
+}
+
+String^ TriangleInfoMap::Serialize(IntPtr dataBuffer, BulletSharp::Serializer^ serializer)
+{
+	const char* name = UnmanagedPointer->serialize(dataBuffer.ToPointer(), serializer->UnmanagedPointer);
+	return gcnew String(name);
+}
+#endif
+
+btScalar TriangleInfoMap::ConvexEpsilon::get()
+{
+	return _triangleInfoMap->m_convexEpsilon;
+}
+void TriangleInfoMap::ConvexEpsilon::set(btScalar value)
+{
+	_triangleInfoMap->m_convexEpsilon = value;
+}
+
+btScalar TriangleInfoMap::EdgeDistanceThreshold::get()
+{
+	return _triangleInfoMap->m_edgeDistanceThreshold;
+}
+void TriangleInfoMap::EdgeDistanceThreshold::set(btScalar value)
+{
+	_triangleInfoMap->m_edgeDistanceThreshold = value;
+}
+
+btScalar TriangleInfoMap::EqualVertexThreshold::get()
+{
+	return _triangleInfoMap->m_equalVertexThreshold;
+}
+void TriangleInfoMap::EqualVertexThreshold::set(btScalar value)
+{
+	_triangleInfoMap->m_equalVertexThreshold = value;
+}
+
+btScalar TriangleInfoMap::PlanarEpsilon::get()
+{
+	return _triangleInfoMap->m_planarEpsilon;
+}
+void TriangleInfoMap::PlanarEpsilon::set(btScalar value)
+{
+	_triangleInfoMap->m_planarEpsilon = value;
+}
+
+int TriangleInfoMap::Size::get()
+{
+	return _triangleInfoMap->size();
+}
+
+btScalar TriangleInfoMap::ZeroAreaThreshold::get()
+{
+	return _triangleInfoMap->m_zeroAreaThreshold;
+}
+void TriangleInfoMap::ZeroAreaThreshold::set(btScalar value)
+{
+	_triangleInfoMap->m_zeroAreaThreshold = value;
 }
 
 btTriangleInfoMap* TriangleInfoMap::UnmanagedPointer::get()
--- src/TriangleInfoMap.h
+++ src/TriangleInfoMap.h
@@ -2,6 +2,8 @@
 
 namespace BulletSharp
 {
+	ref class Serializer;
+
 	public ref class TriangleInfoMap
 	{
 	private:
@@ -9,6 +11,48 @@ namespace BulletSharp
 
 	public:
 		TriangleInfoMap();
+
+		void Clear();
+#ifndef DISABLE_SERIALIZE
+		int	CalculateSerializeBufferSize();
+		String^ Serialize(IntPtr dataBuffer, Serializer^ serializer);
+		//void Deserialize(struct btTriangleInfoMapData& data);
+#endif
+
+		property btScalar ConvexEpsilon
+		{
+			btScalar get();
+			void set(btScalar value);
+		}
+
+		property btScalar EdgeDistanceThreshold
+		{
+			btScalar get();
+			void set(btScalar value);
+		}
+
+		property btScalar EqualVertexThreshold
+		{
+			btScalar get();
+			void set(btScalar value);
+		}
+
+		property btScalar PlanarEpsilon
+		{
+			btScalar get();
+			void set(btScalar value);
+		}
+
+		property int Size
+		{
+			int get();
+		}
+
+		property btScalar ZeroAreaThreshold
+		{
+			btScalar get();
+			void set(btScalar value);
+		}
 
 	internal:
 		property btTriangleInfoMap* UnmanagedPointer
