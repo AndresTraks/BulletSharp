f403b7c725eb5bd4b08e3240dc171e14257a3c82
andres.traks@gmail.com <andres.traks@gmail.com@030a5c38-1ffe-11df-902a-156ec2e33ad3>
2012-06-08 16:50:13 +0000

    Complete ConcaveRaycastDemo.

--- demos/Generic/ConcaveRaycastDemo/ConcaveRaycastDemo.cs
+++ demos/Generic/ConcaveRaycastDemo/ConcaveRaycastDemo.cs
@@ -2,15 +2,188 @@
 using BulletSharp;
 using DemoFramework;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace ConcaveRaycastDemo
 {
+    // Scrolls back and forth over terrain
+    class RaycastBar
+    {
+        static readonly int NUMRAYS_IN_BAR = 100;
+        
+	    Vector3[] source = new Vector3[NUMRAYS_IN_BAR];
+	    Vector3[] dest = new Vector3[NUMRAYS_IN_BAR];
+	    Vector3[] direction = new Vector3[NUMRAYS_IN_BAR];
+	    Vector3[] hit = new Vector3[NUMRAYS_IN_BAR];
+	    Vector3[] normal = new Vector3[NUMRAYS_IN_BAR];
+
+	    int frame_counter;
+	    int ms;
+	    int sum_ms;
+	    int sum_ms_samples;
+	    int min_ms;
+	    int max_ms;
+
+	    float dx;
+	    float min_x;
+	    float max_x;
+	    float min_y;
+	    float max_y;
+	    float sign;
+
+	    public RaycastBar()
+	    {
+		    ms = 0;
+		    max_ms = 0;
+		    min_ms = 9999;
+		    sum_ms_samples = 0;
+		    sum_ms = 0;
+	    }
+
+	    public RaycastBar(bool unused, float ray_length, float min_z, float max_z, float min_y = -10, float max_y = 10)
+	    {
+		    frame_counter = 0;
+		    ms = 0;
+		    max_ms = 0;
+		    min_ms = 9999;
+		    sum_ms_samples = 0;
+		    sum_ms = 0;
+		    dx = 10.0f;
+		    min_x = -40;
+		    max_x = 20;
+		    this.min_y = min_y;
+		    this.max_y = max_y;
+		    sign = 1.0f;
+		    float dalpha = 4*(float)Math.PI/NUMRAYS_IN_BAR;
+		    for (int i = 0; i < NUMRAYS_IN_BAR; i++)
+		    {
+			    float z = (max_z-min_z)/NUMRAYS_IN_BAR * (float)i + min_z;
+			    source[i] = new Vector3(min_x, max_y, z);
+			    dest[i] = new Vector3(min_x + ray_length, min_y, z);
+			    normal[i] = new Vector3(1.0f, 0.0f, 0.0f);
+		    }
+	    }
+
+	    public RaycastBar (float ray_length, float z, float min_y = -1000, float max_y = 10)
+	    {
+		    frame_counter = 0;
+		    ms = 0;
+		    max_ms = 0;
+		    min_ms = 9999;
+		    sum_ms_samples = 0;
+		    sum_ms = 0;
+		    dx = 10.0f;
+		    min_x = -40;
+		    max_x = 20;
+		    this.min_y = min_y;
+		    this.max_y = max_y;
+		    sign = 1.0f;
+		    float dalpha = 4*(float)Math.PI/NUMRAYS_IN_BAR;
+		    for (int i = 0; i < NUMRAYS_IN_BAR; i++)
+		    {
+			    float alpha = dalpha * i;
+			    // rotate around by alpha degrees y 
+			    Matrix tr = Matrix.RotationQuaternion(Quaternion.RotationAxis(new Vector3(0.0f, 1.0f, 0.0f), alpha));
+			    direction[i] = new Vector3(1.0f, 0.0f, 0.0f);
+			    direction[i] = Vector3.TransformCoordinate(direction[i], tr);
+			    direction[i] = direction[i] * ray_length;
+			    source[i] = new Vector3(min_x, max_y, z);
+			    dest[i] = source[i] + direction[i];
+			    dest[i][1] = min_y;
+			    normal[i] = new Vector3(1.0f, 0.0f, 0.0f);
+		    }
+	    }
+
+	    public void Move (float dt)
+	    {
+		    if (dt > (1.0f/60.0f))
+			    dt = 1.0f/60.0f;
+		    for (int i = 0; i < NUMRAYS_IN_BAR; i++)
+		    {
+			    source[i][0] += dx * dt * sign;
+			    dest[i][0] += dx * dt * sign;
+		    }
+		    if (source[0][0] < min_x)
+			    sign = 1.0f;
+		    else if (source[0][0] > max_x)
+			    sign = -1.0f;
+	    }
+
+	    public void Cast (CollisionWorld cw)
+	    {
+#if BATCH_RAYCASTER
+		    if (!gBatchRaycaster)
+			    return;
+
+		    gBatchRaycaster->clearRays ();
+		    for (int i = 0; i < NUMRAYS_IN_BAR; i++)
+		    {
+			    gBatchRaycaster->addRay (source[i], dest[i]);
+		    }
+		    gBatchRaycaster->performBatchRaycast ();
+		    for (int i = 0; i < gBatchRaycaster->getNumRays (); i++)
+		    {
+				    const SpuRaycastTaskWorkUnitOut& out = (*gBatchRaycaster)[i];
+				    hit[i].setInterpolate3(source[i],dest[i],out.HitFraction);
+				    normal[i] = out.hitNormal;
+				    normal[i].Normalize();
+		    }
+#else
+		    for (int i = 0; i < NUMRAYS_IN_BAR; i++)
+		    {
+                using (var cb = new CollisionWorld.ClosestRayResultCallback(source[i], dest[i]))
+                {
+                    cw.RayTest(source[i], dest[i], cb);
+                    if (cb.HasHit)
+                    {
+                        hit[i] = cb.HitPointWorld;
+                        normal[i] = cb.HitNormalWorld;
+                        normal[i].Normalize();
+                    }
+                    else
+                    {
+                        hit[i] = dest[i];
+                        normal[i] = new Vector3(1.0f, 0.0f, 0.0f);
+                    }
+                }
+		    }
+
+		    frame_counter++;
+		    if (frame_counter > 50)
+		    {
+			    min_ms = ms < min_ms ? ms : min_ms;
+			    max_ms = ms > max_ms ? ms : max_ms;
+			    sum_ms += ms;
+			    sum_ms_samples++;
+			    float mean_ms = (float)sum_ms/(float)sum_ms_samples;
+			    Console.WriteLine("%d rays in %d ms %d %d %f", NUMRAYS_IN_BAR * frame_counter, ms, min_ms, max_ms, mean_ms);
+			    ms = 0;
+			    frame_counter = 0;
+		    }
+#endif
+	    }
+
+	    public void Draw(IDebugDraw drawer)
+	    {
+            int i;
+            for (i = 0; i < NUMRAYS_IN_BAR; i++)
+            {
+                drawer.DrawLine(ref source[i], ref hit[i], Color.Lime);
+            }
+            for (i = 0; i < NUMRAYS_IN_BAR; i++)
+            {
+                Vector3 to = hit[i] + normal[i];
+                drawer.DrawLine(ref hit[i], ref to, Color.White);
+            }
+        }
+    }
+
     class ConcaveRaycastDemo : Demo
     {
         Vector3 eye = new Vector3(0, 15, 60);
         Vector3 target = new Vector3(-5, 5, 0);
 
-        DebugDrawModes debugMode = DebugDrawModes.DrawWireframe;
+        DebugDrawModes debugMode = DebugDrawModes.None;
 
         const float TRIANGLE_SIZE = 8.0f;
         const int NUM_VERTS_X = 30;
@@ -21,6 +194,8 @@ namespace ConcaveRaycastDemo
 
         TriangleIndexVertexArray indexVertexArrays;
         BvhTriangleMeshShape groundShape;
+        static RaycastBar raycastBar;
+        RigidBody staticBody;
 
         protected override void OnInitialize()
         {
@@ -57,6 +232,9 @@ namespace ConcaveRaycastDemo
             indices.Dispose();
 
             indexVertexArrays.AddIndexedMesh(mesh);
+
+            raycastBar = new RaycastBar(4000.0f, 0.0f);
+            //raycastBar = new RaycastBar(true, 40.0f, -50.0f, 50.0f);
         }
 
         void SetVertexPositions(float waveheight, float offset)
@@ -116,7 +294,7 @@ namespace ConcaveRaycastDemo
             groundShape = new BvhTriangleMeshShape(indexVertexArrays, useQuantizedAabbCompression);
             CollisionShapes.Add(groundShape);
 
-            RigidBody staticBody = LocalCreateRigidBody(0.0f, Matrix.Identity, groundShape);
+            staticBody = LocalCreateRigidBody(0.0f, Matrix.Identity, groundShape);
             staticBody.CollisionFlags |= CollisionFlags.StaticObject;
         }
 
@@ -127,7 +305,20 @@ namespace ConcaveRaycastDemo
                 offset += FrameDelta;
                 SetVertexPositions(waveHeight, offset);
                 Graphics.MeshFactory.RemoveShape(groundShape);
+
+                Vector3 worldMin = new Vector3(-1000,-1000,-1000);
+		        Vector3 worldMax = new Vector3(1000,1000,1000);
+
+		        groundShape.RefitTree(worldMin, worldMax);
+
+                //clear all contact points involving mesh proxy. Note: this is a slow/unoptimized operation.
+                Broadphase.OverlappingPairCache.CleanProxyFromPairs(staticBody.BroadphaseHandle, Dispatcher);
             }
+
+            raycastBar.Move(FrameDelta);
+            raycastBar.Cast(World);
+            raycastBar.Draw(World.DebugDrawer);
+
             base.OnUpdate();
         }
 
@@ -136,6 +327,16 @@ namespace ConcaveRaycastDemo
             if (Input.KeysPressed.Contains(Keys.G))
             {
                 animatedMesh = !animatedMesh;
+                if (animatedMesh)
+                {
+                    staticBody.CollisionFlags |= CollisionFlags.KinematicObject;
+                    staticBody.ActivationState = ActivationState.DisableDeactivation;
+                }
+                else
+                {
+                    staticBody.CollisionFlags &= ~CollisionFlags.KinematicObject;
+                    staticBody.ActivationState = ActivationState.ActiveTag;
+                }
             }
             base.OnHandleInput();
         }
