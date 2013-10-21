1cd738d766297ea819b5e0101225cb9522a7b982
andres.traks@gmail.com <andres.traks@gmail.com@030a5c38-1ffe-11df-902a-156ec2e33ad3>
2013-10-21 16:47:40 +0000

    Add Visual Studio 2013 solution.

---
+++ vs2013/build_all.bat
@@ -0,0 +1,23 @@
+@echo off
+set MSBUILD=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+
+if "[%ProgramFiles(x86)%]"=="[]" goto x86
+set VCPATH=%ProgramFiles(x86)%
+goto checkdone
+:x86
+set VCPATH=%ProgramFiles%
+:checkdone
+
+set VCPATH="%VCPATH%\Microsoft Visual Studio 12.0\VC\vcpackages"
+set OPTS=BulletSharp.vcxproj /p:VCBuildToolPath=%VCPATH% /p:VisualStudioVersion=12.0
+
+%MSBUILD% %OPTS% /p:Configuration="Release Axiom"
+%MSBUILD% %OPTS% /p:Configuration="Release Generic"
+%MSBUILD% %OPTS% /p:Configuration="Release Mogre"
+%MSBUILD% %OPTS% /p:Configuration="Release OpenTK"
+%MSBUILD% %OPTS% /p:Configuration="Release SharpDX"
+%MSBUILD% %OPTS% /p:Configuration="Release SlimDX"
+%MSBUILD% %OPTS% /p:Configuration="Release XNA 3.1"
+%MSBUILD% %OPTS% /p:Configuration="Release XNA 4.0"
+%MSBUILD% %OPTS% /p:Configuration="Release SlimMath"
+rem %MSBUILD% %OPTS% /p:Configuration="Release Windows API Code Pack"
