2e46d3d6385028f7d56dc9adca16f7d1c939355c
andres.traks@gmail.com <andres.traks@gmail.com@030a5c38-1ffe-11df-902a-156ec2e33ad3>
2013-11-11 17:34:50 +0000

    Add MonoGame demo.

---
+++ demos/MonoGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace BasicDemo
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            try
+            {
+                Assembly.Load("MonoGame.Framework, Version=3.0.1.0, Culture=neutral, PublicKeyToken=null");
+            }
+            catch
+            {
+                MessageBox.Show("MonoGame 3.0.1 redistributable not installed.", "Error!");
+                return;
+            }
+
+            try
+            {
+                Assembly.Load("BulletSharp");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "BulletSharp Error!");
+                return;
+            }
+
+            using (BasicDemo game = new BasicDemo())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
---
+++ demos/MonoGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BasicDemo")]
+[assembly: AssemblyProduct("BasicDemo")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © Andres Traks 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51d40407-8e8b-416b-83e7-fa99e5e362da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
