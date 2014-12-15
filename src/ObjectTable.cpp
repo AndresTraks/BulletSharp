be76c6f251a670b0946d293a8a3aaec6ec6f3d7f
George Papadopoulos <virusfree21@gmail.com>
2014-12-15 18:36:26 +0200

    Mufti-threading protection for ObjectTable operations

--- src/ObjectTable.cpp
+++ src/ObjectTable.cpp
@@ -2,6 +2,8 @@
 
 #include "ObjectTable.h"
 
+using namespace System::Threading;
+
 static BulletSharp::ObjectTable::ObjectTable()
 {
 	objectTable = gcnew Dictionary<intptr_t, Object^>();
@@ -9,34 +11,72 @@ static BulletSharp::ObjectTable::ObjectTable()
 
 void BulletSharp::ObjectTable::Add(Object^ obj, void* unmanagedPointer)
 {
-	objectTable->Add((intptr_t)unmanagedPointer, obj);
+	Monitor::Enter(objectTable);
+	try
+	{
+		if (!objectTable->ContainsKey((intptr_t)unmanagedPointer))
+			objectTable->Add((intptr_t)unmanagedPointer, obj);
+		else
+		{
+			//something when wrong here, so inform in debug mode
+			Debug:assert(false, "ObjectTable key already exists.");
+			//key already exists.. try to handle this by updating/overwriting key entry to newest value
+			//this should protect us against a wrapper that did not unregister from object table, and race conditions that try to write the same key.
+			objectTable[(intptr_t)unmanagedPointer] = obj;
+		}
+	}
+	finally{ Monitor::Exit(objectTable); }
 }
 
 bool BulletSharp::ObjectTable::Contains(intptr_t unmanagedPointer)
 {
-	return objectTable->ContainsKey(unmanagedPointer);
+	Monitor::Enter(objectTable);
+	try
+	{
+		return objectTable->ContainsKey(unmanagedPointer);
+	}
+	finally{ Monitor::Exit(objectTable); }
 }
 
 bool BulletSharp::ObjectTable::Contains(Object^ managedObject)
 {
-	return objectTable->ContainsValue(managedObject);
+	Monitor::Enter(objectTable);
+	try
+	{
+		return objectTable->ContainsValue(managedObject);
+	}
+	finally{ Monitor::Exit(objectTable); }
 }
 
 void BulletSharp::ObjectTable::Remove(void* unmanagedPointer)
 {
-	objectTable->Remove((intptr_t)unmanagedPointer);
+	Monitor::Enter(objectTable);
+	try
+	{
+		objectTable->Remove((intptr_t)unmanagedPointer);
+	}
+	finally{ Monitor::Exit(objectTable); }
 }
 
-generic<class T>
-T BulletSharp::ObjectTable::GetObject(intptr_t unmanagedPointer)
+generic<class T> T BulletSharp::ObjectTable::GetObject(intptr_t unmanagedPointer)
 {
-	return (T)objectTable[unmanagedPointer];
+	Monitor::Enter(objectTable);
+	try
+	{
+		return (T)objectTable[unmanagedPointer];
+	}
+	finally{ Monitor::Exit(objectTable); }
 }
 
 intptr_t BulletSharp::ObjectTable::GetUnmanagedObject(Object^ unmanagedPointer)
 {
-	for each(intptr_t key in objectTable->Keys)
-		if (objectTable[key] == unmanagedPointer)
-			return key;
-	return 0;
+	Monitor::Enter(objectTable);
+	try
+	{
+		for each(intptr_t key in objectTable->Keys)
+			if (objectTable[key] == unmanagedPointer)
+				return key;
+		return 0;
+	}
+	finally{ Monitor::Exit(objectTable); }
 }
